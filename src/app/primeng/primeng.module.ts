3840cf6b0854be7e74b366e9662b9049aaba4387
Felipe Rafael Montenegro Morriberon <flashgames104@gmail.com>
2023-12-27 00:33:41 -0500

    202312270032

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ButtonModule } from "primeng/button";
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  imports: [],
+  exports:[
+    ButtonModule
   ]
 })
 export class PrimengModule { }
